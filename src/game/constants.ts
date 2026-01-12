0e1f273f68d65ce98fb4fec9f204daeb01b865c7
George Hollister <george@and-element.com>
2026-01-12 15:47:15 +0000

    fix: tweek sizes to work better on screen sizes

--- src/game/constants.ts
+++ src/game/constants.ts
@@ -13,16 +13,16 @@ export function getGameConfig() {
 		// Central circle
 		CIRCLE_CENTER_X: width / 2,
 		CIRCLE_CENTER_Y: height + height * 0.1, // 10% of screen height offset
-		CIRCLE_RADIUS: minDimension * 0.15,
+		CIRCLE_RADIUS: width * 0.4,
 
 		// Defense wall
-		WALL_DISTANCE: minDimension * 0.25,
+		WALL_DISTANCE: width * 0.6,
 		WALL_ARC_ANGLE: Math.PI / 5,
 		WALL_THICKNESS: minDimension * 0.008, // ~0.8% of min dimension
 
 		// Particles
-		PARTICLE_RADIUS: minDimension * 0.0005, // ~0.05% of min dimension
-		PARTICLE_SPEED: minDimension * 0.002, // ~0.2% of min dimension per frame
+		PARTICLE_RADIUS: minDimension * 0.002, // ~0.05% of min dimension
+		PARTICLE_SPEED: height * 0.002, // ~0.2% of min dimension per frame
 		PARTICLE_SPAWN_INTERVAL: 1000,
 		PARTICLE_SPAWN_DECREASE: 20,
 		MIN_SPAWN_INTERVAL: 200,
